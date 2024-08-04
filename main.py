479ad49b6ae636c5915d53b7d1fc79860a211dc7
Jared Vizzi <jared.vizzi@gmail.com>
2024-08-04 17:01:01 -0400

    Paths show in entry boxes after selecting

--- main.py
+++ main.py
@@ -2,31 +2,24 @@ import tkinter as tk
 from tkinter import filedialog
 import tkinter.ttk as ttk
 import script
-
-
-class Foo:
-    def __init__(self):
-        self.input = None
-        self.output = None
-
-
-foo = Foo()
+input_dir = ""
+output_dir = ""
 
 
 def input_dialog():
+    global input_dir
     input_dir = filedialog.askdirectory()
-    foo.input = input_dir
-    print(input_dir)
+    input_entry.insert("end", input_dir)
 
 
 def output_dialog():
+    global output_dir
     output_dir = filedialog.askdirectory()
-    foo.output = output_dir
-    print(output_dir)
+    output_entry.insert("end", output_dir)
 
 
 def submit():
-    script.main(foo.input, foo.output)
+    script.main(input_dir, output_dir)
 
 
 root = tk.Tk()
@@ -37,7 +30,7 @@ root.config(padx=25, pady=25)
 input_label = ttk.Label(text="Input Directory:")
 input_label.grid(column=0, row=0)
 
-input_entry = ttk.Entry(width=25)
+input_entry = tk.Entry(width=25)
 input_entry.grid(column=1, row=0)
 
 in_button = ttk.Button(text="Browse", command=input_dialog)
@@ -46,9 +39,8 @@ in_button.grid(column=2, row=0)
 output_label = ttk.Label(text="Output Directory:")
 output_label.grid(column=0, row=1)
 
-output_entry = ttk.Entry(width=25)
+output_entry = tk.Entry(width=25)
 output_entry.grid(column=1, row=1)
-
 
 out_button = ttk.Button(text="Browse", command=output_dialog)
 out_button.grid(column=2, row=1)
